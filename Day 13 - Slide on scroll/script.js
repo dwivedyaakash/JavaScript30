8de2ca7fe304f77afcac0f453cf8065f66c71fac
Aakash Dwivedy <aakashdwd07@gmail.com>
2021-09-13 11:58:45 +0530

    day 13

---
+++ Day 13 - Slide on scroll/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    // halfway to image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
