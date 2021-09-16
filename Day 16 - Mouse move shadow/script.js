49a7dd9000e2e5ea1bbbe57a879205a79177bcce
Aakash Dwivedy <aakashdwd07@gmail.com>
2021-09-16 22:27:19 +0530

    day 16

---
+++ Day 16 - Mouse move shadow/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 100;
+
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
+}
+
+hero.addEventListener("mousemove", shadow);
